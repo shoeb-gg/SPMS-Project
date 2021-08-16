474cec3d01f8de7b564b8a8eecfee9237326314e
shoeb-gg <1920038@iub.edu.bd>
2021-08-17 02:33:48 +0600

    trying out basic functionality of inputing raw sql into django

--- spm/performance_monitor/views.py
+++ spm/performance_monitor/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from performance_monitor.models import Cse303ObeMarkSheetMarks
+
+
 # Create your views here.
+def showStudents(request):
+    students = Cse303ObeMarkSheetMarks.objects.raw("SELECT * FROM `cse303_obe_mark_sheet__marks_`")
+
+    
+    return render(request, 'studentList.html', {'map': students})
+     
