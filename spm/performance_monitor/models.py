81c2306d2d7667be0b5ed8a76aa62bc055bcce94
Istiak <1930038@iub.edu.bd>
2021-08-24 12:06:03 -0700

    Add new model in model.py

--- spm/performance_monitor/models.py
+++ spm/performance_monitor/models.py
@@ -77,3 +77,150 @@ class Meta:
         managed = False
         db_table = 'cse303_obe_mark_sheet__verdict_'
 
+class University_T(models.Model):
+    universityName = models.CharField(max_length=50, primary_key=True)
+    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.universityName
+        
+
+class School_T(models.Model):
+    schoolID = models.CharField(max_length=5, primary_key=True)
+    schoolName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.schoolID
+
+
+class Department_T(models.Model):
+    departmentID = models.CharField(max_length=5, primary_key=True)
+    departmentName = models.CharField(max_length=50)
+    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.departmentID
+
+
+class Program_T(models.Model):
+    programID = models.AutoField(primary_key=True)
+    programName = models.CharField(max_length=70)
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE, default='N/A')
+
+    def __str__(self):
+        return self.programName
+
+
+
+
+class Student_T(models.Model):
+    studentID = models.CharField(max_length=7, primary_key=True)
+    studentName = models.CharField(max_length=50, null=True)
+    dateOfBirth = models.DateField(null=True)
+    gender = models.CharField(max_length=6, null=True)
+    email = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    address = models.CharField(max_length=50, null=True)
+    enrollmentDate = models.DateField(null=True)
+    university = models.ForeignKey(University_T, on_delete=models.CASCADE)
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE, default='N/A')
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.studentID   
+
+class Faculty_T(Employee_T):
+    facultyID = models.IntegerField(primary_key=True)
+    facultyName = models.CharField(max_length=50, null=True)
+    startDate = models.DateField(null=True)
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.faculty
+
+
+class Course_T(models.Model):
+    courseID = models.CharField(max_length=7, primary_key=True)
+    courseName = models.CharField(max_length=50, null=True)
+    numOfCredits = models.DecimalField(max_digits=2, decimal_places=1)
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE)
+    courseType = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.courseID
+
+
+class PrereqCourse_T(models.Model):
+    course = models.ForeignKey(Course_T, on_delete=models.CASCADE, related_name='Course')
+    preReqCourse = models.ForeignKey(Course_T, on_delete=models.CASCADE, related_name='PreRequisite')
+
+
+class PLO_T(models.Model):
+    ploID = models.AutoField(primary_key=True)
+    ploNum = models.CharField(max_length=5)
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE)
+    details = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.ploNum
+
+
+class Section_T(models.Model):
+    sectionID = models.AutoField(primary_key=True)
+    sectionNum = models.IntegerField()
+    course = models.ForeignKey(Course_T, on_delete=models.CASCADE)
+    faculty = models.ForeignKey(Faculty_T, on_delete=models.CASCADE)
+    semester = models.CharField(max_length=15)
+
+
+    def __str__(self):
+        return str(self.sectionNum)
+
+
+class Enrollment_T(models.Model):
+    enrollmentID = models.AutoField(primary_key=True)
+    student = models.ForeignKey(Student_T, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section_T, on_delete=models.CASCADE)
+    semester = models.CharField(max_length=15)
+    year = models.IntegerField(default=2020,null=True)
+
+    def __str__(self):
+        return str(self.registrationID)
+
+
+
+class CO_T(models.Model):
+    coID = models.AutoField(primary_key=True)
+    coNum = models.CharField(max_length=4)
+    plo = models.ForeignKey(PLO_T, on_delete=models.CASCADE, default='N/A')
+    course = models.ForeignKey(Course_T, on_delete=models.CASCADE, default='N/A')
+    thresold = models.FloatField(default=40)
+
+    def __str__(self):
+        return self.coNum
+
+
+class Assessment_T(models.Model):
+    assessmentID = models.AutoField(primary_key=True)
+    assessmentName = models.CharField(max_length=30)
+    questionNum = models.IntegerField()
+    totalMarks = models.FloatField()
+    co = models.ForeignKey(CO_T, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section_T, on_delete=models.CASCADE)
+    weight = models.FloatField()
+
+    def __str__(self):
+        return self.assessmentName + " "+str(self.questionNum)
+
+
+
+class Evaluation_T(models.Model):
+    evaluationID = models.AutoField(primary_key=True)
+    obtainedMarks = models.FloatField()
+    assessment = models.ForeignKey(Assessment_T, on_delete=models.CASCADE)
+    enrollment = models.ForeignKey(Registration_T, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.evaluationID)
+
+          
