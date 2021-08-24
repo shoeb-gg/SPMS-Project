1bd2c24d226fbc61a728511dc7d718f1115a44ec
Istiak <1930038@iub.edu.bd>
2021-08-24 10:08:26 -0700

    Add new models in model.py

--- spm/performance_monitor/models.py
+++ spm/performance_monitor/models.py
@@ -7,7 +7,6 @@ from django.db import models
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
 
 
 class Cse303ObeMarkSheetAnalysis(models.Model):
@@ -74,6 +73,138 @@ class Cse303ObeMarkSheetVerdict(models.Model):
     outcomefailedtoachieve = models.CharField(db_column='outcomeFailedToAchieve', max_length=5, blank=True, null=True)  # Field name made lowercase.
     outcomefailedtoachieve_field = models.CharField(db_column='outcomeFailedToAchieve(%)', max_length=10, blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
 
-    class Meta:
+class Meta:
         managed = False
         db_table = 'cse303_obe_mark_sheet__verdict_'
+
+class University_T(models.Model):
+    universityName = models.CharField(max_length=50, primary_key=True)
+    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.universityname
+
+class School_T(models.Model):
+    schoolID = models.CharField(max_length=5, primary_key=True)
+    schoolName = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.schoolID
+    
+class Department_T(models.Model):
+    departmentID = models.CharField(max_length=5, primary_key=True)
+    departmentName = models.CharField(max_length=30)
+    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.departmentID   
+
+class Program_T(models.Model):
+    programID = models.CharField(max_length=5, primary_key=True)
+    programName = models.CharField(max_length=30)
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE, default='N/A')
+    
+    def __str__(self):
+        return self.programName
+
+class Student_T(models.Model):
+    studentID = models.CharField(max_length=7, primary_key=True)
+    studentName = models.CharField(max_length=30, null=True)
+    university = models.ForeignKey(University_T, on_delete=models.CASCADE)
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE, default='N/A')
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+    dateOfBirth = models.DateField(null=True)
+    gender = models.CharField(max_length=1, null=True)
+    email = models.CharField(max_length=30, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    address = models.CharField(max_length=30, null=True)
+    
+    def __str__(self):
+        return self.studentID
+
+class Faculty_T(models.Model):
+    facultyID = models.CharField(max_length=4, primary_key=True)
+    facultyName = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1, null=True)
+    dateOfBirth = models.DateField(null=True)
+    email = models.CharField(max_length=30, null=True)
+    phone = models.CharField(max_length=15, null=True)
+    address = models.CharField(max_length=30, null=True)
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.facultyName
+
+class Course_T(models.Model):
+    courseID = models.CharField(max_length=7, primary_key=True)
+    courseName = models.CharField(max_length=30, null=True)
+    noOfCredits = models.IntegerField()
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.courseID
+
+class PrereqCourse_T(models.Model):
+    course = models.ForeignKey(Course_T, on_delete=models.CASCADE, related_name='Course')
+    preReqCourse = models.ForeignKey(Course_T, on_delete=models.CASCADE, related_name='PreRequisite')
+
+    
+class PLO_T(models.Model):
+    ploID = models.AutoField(primary_key=True)
+    ploNum = models.CharField(max_length=5)
+    program = models.ForeignKey(Program_T, on_delete=models.CASCADE)
+    details = models.CharField(max_length=50)
+  
+    def __str__(self):
+        return self.ploNum    
+    
+class CO_T(models.Model):
+    coID = models.AutoField(primary_key=True)
+    coNum = models.CharField(max_length=4)
+    plo = models.ForeignKey(PLO_T, on_delete=models.CASCADE, default='N/A')
+    course = models.ForeignKey(Course_T, on_delete=models.CASCADE, default='N/A')
+    thresold = models.FloatField(default=40)
+    
+    def __str__(self):
+        return self.coNum    
+
+
+class Section_T(models.Model):
+    sectionID = models.AutoField(primary_key=True)
+    sectionNum = models.IntegerField()
+    course = models.ForeignKey(Course_T, on_delete=models.CASCADE)
+    faculty = models.ForeignKey(Faculty_T, on_delete=models.CASCADE)
+    semester = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return str(self.sectionNum)    
+    
+class Enrollment_T(models.Model):
+    enrollmentID = models.AutoField(primary_key=True)
+    student = models.ForeignKey(Student_T, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section_T, on_delete=models.CASCADE, default=0)
+    semester = models.CharField(max_length=6)
+    year = models.CharField(max_length=4)
+    
+    def __str__(self):
+        return str(self.enrollmentID)    
+    
+class Assessment_T(models.Model):
+    assessmentNo = models.AutoField(primary_key=True)
+    assessmentName = models.CharField(max_length=30)
+    questionNum = models.IntegerField()
+    marks = models.FloatField()
+    co = models.ForeignKey(CO_T, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section_T, on_delete=models.CASCADE, default=0)
+ 
+    def __str__(self):
+        return self.assessmentName + " "+str(self.questionNum)
+
+class Evaluation_T(models.Model):
+    evaluationID = models.AutoField(primary_key=True)
+    obtainedMarks = models.FloatField()
+    assessment = models.ForeignKey(Assessment_T, on_delete=models.CASCADE, default='N/A')
+    enrollment = models.ForeignKey(Enrollment_T, on_delete=models.CASCADE, default=0)
+
+    def __str__(self):
+        return str(self.evaluationID)
